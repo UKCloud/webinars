27afa361a6d1c1c5f7c2e5b28c2f89002b1d829f
unknown <rcoward@SED00187.internal.local>
2016-03-21 10:53:47 +0000

    Updated webserver vcd_network CIDR

--- Portal_and_API_Overview/terraform/main.tf
+++ Portal_and_API_Overview/terraform/main.tf
@@ -6,7 +6,7 @@ variable "vapp_template"  { default = "centos71" }
 variable "edge_gateway"   { default = "Edge Gateway Name" }
 variable "jumpbox_ext_ip" {}
 variable "mgt_net_cidr"   { default = "10.10.0.0/24" }
-variable "web_net_cidr"   { default = "10.20.0.0/24" }
+variable "web_net_cidr"   { default = "10.200.0.0/24" }
 variable "jumpbox_int_ip" { default = "10.10.0.100" }
 variable "haproxy_int_ip" { default = "10.20.0.10" }
 variable "webserver_count" { default = 2 }
